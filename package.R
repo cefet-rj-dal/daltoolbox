e01565bced78b36d2c25d71200579a6729d0ae9e
eogasawara <eogasawara@eic.cefet-rj.br>
2024-11-11 16:32:51 -0300

    Update

--- package.R
+++ package.R
@@ -56,8 +56,8 @@ if (FALSE) { #build package for cran
   pkgbuild::build(manual = TRUE)
 
   #run in terminal
-  #R CMD check daltoolbox_1.0.787.tar.gz
-  #R CMD check daltoolbox_1.0.787.tar.gz --as-cran
+  #R CMD check daltoolbox_1.01.707.tar.gz
+  #R CMD check daltoolbox_1.01.707.tar.gz --as-cran
 
   #upload package
   #https://cran.r-project.org/submit.html
