3cc9f790fd0c34416130345b16ed87a2ae1c0ea1
Janio de Souza Lima <56620033+janiosl@users.noreply.github.com>
2024-11-17 10:37:06 -0300

    Update trans_sae_encode_decode.R

--- R/trans_sae_encode_decode.R
+++ R/trans_sae_encode_decode.R
@@ -8,49 +8,42 @@
 #'@param num_epochs number of epochs for training
 #'@param learning_rate learning rate
 #'@param k_ae number of AE layers in the stack
-#'@return returns a `sae_encode_decode` object.
+#'@return a `sae_encode_decode` object.
 #'@examples
-#'#See an example of using `sae_encode_decode` at this
-#'#[link](https://github.com/cefet-rj-dal/daltoolbox/blob/main/transf/sae_enc_decode.ipynb)
+#'#See example at https://nbviewer.org/github/cefet-rj-dal/daltoolbox-examples
 #'@import reticulate
 #'@export
-sae_encode_decode <- function(input_size, encoding_size, batch_size = 32, num_epochs = 1000, learning_rate = 0.001, k_ae=3) {
+sae_encode_decode <- function(input_size, encoding_size, batch_size = 32, num_epochs = 1000, learning_rate = 0.001, k=3) {
   obj <- dal_transform()
   obj$input_size <- input_size
   obj$encoding_size <- encoding_size
   obj$batch_size <- batch_size
   obj$num_epochs <- num_epochs
   obj$learning_rate <- learning_rate
-  obj$k_ae <- k_ae
+  obj$k <- k
   class(obj) <- append("sae_encode_decode", class(obj))
-
+  
   return(obj)
 }
 
 #'@export
-fit.sae_encode_decode <- function(obj, data, return_loss=FALSE, ...) {
+fit.sae_encode_decode <- function(obj, data, ...) {
   if (!exists("sae_create"))
     reticulate::source_python(system.file("python", "sae_autoencoder.py", package = "daltoolbox"))
-
+  
   if (is.null(obj$model))
-    obj$model <- sae_create(obj$input_size, obj$encoding_size, obj$k_ae)
-
-  if (return_loss){
-    fit_output <- sae_fit(obj$model, data, num_epochs = obj$num_epochs, learning_rate = obj$learning_rate, return_loss=return_loss)
-    obj$model <- fit_output[[1]]
-
-    return(list(obj=obj, loss=fit_output[-1]))
-  }else{
-    obj$model <- sae_fit(obj$model, data, num_epochs = obj$num_epochs, learning_rate = obj$learning_rate, return_loss=return_loss)
-    return(obj)
-  }
+    obj$model <- sae_create(obj$input_size, obj$encoding_size, obj$k)
+  
+  obj$model <- sae_fit(obj$model, data, num_epochs = obj$num_epochs, learning_rate = obj$learning_rate)
+  
+  return(obj)
 }
 
 #'@export
 transform.sae_encode_decode <- function(obj, data, ...) {
   if (!exists("sae_create"))
     reticulate::source_python(system.file("python", "sae_autoencoder.py", package = "daltoolbox"))
-
+  
   result <- NULL
   if (!is.null(obj$model))
     result <- sae_encode_decode(obj$model, data)
