9f8017d6ac63b17c415b41f73bdc4eae5dcd8aa8
lucasgiutavares <lucasgiutavares@gmail.com>
2025-01-01 17:56:30 -0300

    fixed_zscore fix

--- R/trans_norm_fixed_zscore.R
+++ R/trans_norm_fixed_zscore.R
@@ -34,7 +34,7 @@ fit.fixed_zscore <- function(obj, data, ...) {
   }
   
   obj$zmodel <- zscore()
-  obj$minmax_model <- minmax()
+  obj$minmax_model <- minmax(minmax_list=minmax_list)
   obj$zmodel <- fit(obj$zmodel, data)
   z <- transform(obj$zmodel, data)
   obj$minmax_model <- fit(obj$minmax_model, z)
