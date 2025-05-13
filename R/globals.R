a7daadf47e7007b53fe430e5aad1d76ef8a4a2e4
eogasawara@ieee.org <eogasawara@ieee.org>
2025-05-13 00:03:05 -0300

    update

--- R/globals.R
+++
@@ -1,9 +0,0 @@
-utils::globalVariables(c("ts_lstm_create", "ts_lstm_fit", "ts_lstm_predict",
-                         "ts_conv1d_create","ts_conv1d_fit", "ts_conv1d_predict",
-                         "autoenc_create", "autoenc_fit", "autoenc_encode", "autoenc_encode_decode",
-                         "autoenc_adv_create", "autoenc_adv_fit", "autoenc_adv_encode", "autoenc_adv_encode_decode",
-                         "autoenc_conv_create", "autoenc_conv_fit", "autoenc_conv_encode", "autoenc_conv_encode_decode",
-                         "autoenc_lstm_create", "autoenc_lstm_fit", "autoenc_lstm_encode", "autoenc_lstm_encode_decode",
-                         "autoenc_stacked_create", "autoenc_stacked_fit", "autoenc_stacked_encode", "autoenc_stacked_encode_decode",
-                         "autoenc_variational_create", "autoenc_variational_fit", "autoenc_variational_encode", "autoenc_variational_encode_decode",
-                         "autoenc_denoise_create", "autoenc_denoise_fit", "autoenc_denoise_encode", "autoenc_denoise_encode_decode"))
